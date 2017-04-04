74154591c116acd881f3fb16918ba3593ff6a1e7
Picojazz <picojazzz@gmail.com>
2017-04-04 14:20:52 +0000

    remove inscription page

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 				<div class ="card-panel z-depth-5 ">
 					<ul  class="tabs tabs-fixed-width ">
         				<li class="tab col s3 "><a class="signin" href="#signin"> <span class="blue-text ">Se connecter</span></a></li>
-        				<li class="tab col s3 "><a class="signup" href="#signup"> <span class="blue-text ">S'inscrire</span></a></li>
+        				<!--<li class="tab col s3 "><a class="signup" href="#signup"> <span class="blue-text ">S'inscrire</span></a></li>-->
       				</ul>
       					<form method="post" action="php/moduleAuthentification.php" >
 						<div id="signin" >
@@ -49,37 +49,7 @@
     						<button type="submit" name="connect" class="btn blue waves-effect waves-light">Se connecter</button>
     					</div>
     					</form>
-						<form method="post" action="php/moduleAuthentification.php" enctype="multipart/form-data">
-    					<div id="signup" >
-      				    	<div class="input-field">
-    							<label for="">Prenom</label>
-    							<input type="text" name="prenom" required>
-    						</div>
-    						<div class="input-field">
-    							<label for="">Nom</label>
-    							<input type="text" name="nom" required>
-    						</div>
-    						
-    						<div class="input-field">
-    							<label for="">Identifiant</label>
-    							<input type="text" name="login" required>
-    						</div>
-    						<div class="input-field">
-    							<label for="">Mot De Passe</label>
-    							<input type="password" name="password" >
-    						</div>
-    						<div class="file-field input-field">
-								<div class   ="btn blue">
-									<span>Photo</span>
-									<input  type  ="file" name="photo">
-								</div>
-								<div class   ="file-path-wrapper">
-									<input class ="file-path validate " type="text">
-								</div>
-							</div>
-    						<button type="submit" name="signup" class="btn blue waves-effect waves-light">S'inscrire</button>
-    						</form>
-      				    </div>
+						
 				</div>
 
 				
